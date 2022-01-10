6438a58a6436a762a7534e85fdd525826e13aaeb
Sharmila Yadav <ydv.sharmila@gmail.com>
2022-01-10 23:31:29 +0200

    Using material ui component to create right profile

--- src/components/RightProfile.js
+++ src/components/RightProfile.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './css/dashboard.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const RightProfile = props => {
 
@@ -13,52 +16,18 @@ const RightProfile = props => {
                 <div>
                     <h5>Upcoming Reservation Details</h5>
                 </div>
-                <div>
-                    <h5 className='h5'>City:</h5>
-                    <input
-                        className='input-right'
-                        placeholder="Turku"
-                    />
-                </div>
-                <div>
-                    <h5 className='h5'>Building:</h5>
-                    <input
-                        className='input-right'
-                        placeholder="Main Building"
-                    />
-                </div>
-                <div>
-                    <h5 className='h5'>Room:</h5>
-                    <input
-                        className='input-right'
-                        placeholder="Romeo"
-                    />
-                </div>
-                <div>
-                    <h5 className='h5'>Spot:</h5>
-                    <input
-                        className='input-right'
-                        placeholder="whole Room"
-                    />
-                </div>
-                <div>
-                    <h5 className='h5'>Date:</h5>
-                    <input
-                        className='input-right'
-                        placeholder="10/01/2022, Monday"
-                    />
-                </div>
-                <div className='flexbox-container-buttons'>
-                    <button className='button' onClick={clickMe}>
-                        Add new
-                    </button>
-                    <button className='button' onClick={clickMe}>
-                        Modify
-                    </button>
-                    <button className='button' onClick={clickMe}>
-                        Delete
-                    </button>
-                </div>
+
+                <Stack spacing={2} direction="column">
+                    <TextField id="standard-basic-1" label="City:" variant="standard" defaultValue="helsinki" />
+                    <TextField id="standard-basic-2" label="Building:" variant="standard"  defaultValue="Main Building" />
+                    <TextField id="standard-basic-3" label="Room" variant="standard"  defaultValue="Romeo" />
+                    <TextField id="standard-basic-4" label="Spot:" variant="standard"  defaultValue="Whole Rome" />
+                    <Stack spacing={2} direction="row">
+                        <Button onClick={clickMe} variant="contained">Add new</Button>
+                        <Button onClick={clickMe} variant="contained">Modify</Button>
+                        <Button onClick={clickMe} variant="outlined">Delete</Button>
+                    </Stack>
+                </Stack>
             </div>
         </div>
     );
